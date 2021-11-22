56347ec7f2efaf5641887ee2776fb370c17b9a5d
Shawn Yang <shawn.yang@idtecproducts.com>
2021-11-22 19:45:50 +0800

    Temp commit before go home

---
+++ poc-api-oauth2-resource-server/src/test/java/idv/shawnyang/ApplicationTests.java
@@ -0,0 +1,13 @@
+package idv.shawnyang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
