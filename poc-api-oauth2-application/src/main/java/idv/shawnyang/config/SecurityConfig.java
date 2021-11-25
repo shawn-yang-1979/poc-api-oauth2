5311dc069d97a3efef09201f1a68d9219497effb
shawn-yang-1979 <ocelot.yang.1979@gmail.com>
2021-11-25 23:57:46 +0800

    temp commit

--- poc-api-oauth2-application/src/main/java/idv/shawnyang/config/SecurityConfig.java
+++ poc-api-oauth2-application/src/main/java/idv/shawnyang/config/SecurityConfig.java
@@ -1,26 +1,22 @@
 package idv.shawnyang.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-	@Autowired
-	private UserDetailsService userDetailsService;
-
-	@Autowired
-	private PasswordEncoder passwordEncoder;
-
 	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/").permitAll().anyRequest().authenticated();
+		http.exceptionHandling().authenticationEntryPoint((request, response, authException) -> {
+			response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
+		});
 	}
 
 }
